1ab5edf9d975d886ec457b30389f36d16a0eac2c
tugceacar <tugceacar.ozkan@gmail.com>
2021-08-18 14:47:07 -0400

    change

--- src/BugLife.java
+++ src/BugLife.java
@@ -2,5 +2,7 @@ public class BugLife {
 
     public static void main(String[] args) {
         System.out.println("I like nature..");
+        System.out.println("I like ocean");
+        System.out.println("I like summer");
     }
 }
