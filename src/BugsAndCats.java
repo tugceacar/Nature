20da12de7a79cd06c10a1cb3493b7fd559174ae4
tugceacar <88342928+tugceacar@users.noreply.github.com>
2021-08-18 10:31:02 -0400

    Updated

--- src/BugsAndCats.java
+++ src/BugsAndCats.java
@@ -2,5 +2,10 @@ public class BugsAndCats {
 
     public static void main(String[] args) {
         System.out.println("Ocean..");
+        System.out.println("Ocean..");
+        System.out.println("Ocean..");
+        System.out.println("Ocean..");
+        System.out.println("Ocean..");
+        System.out.println("Ocean..");
     }
 }
